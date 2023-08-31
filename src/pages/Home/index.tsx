ef9b08d8a56df0912c028fa768c6d27505e67c44
duardoqueiroz <eduardoldq1@gmail.com>
2023-08-31 16:47:56 -0300

    finish

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,100 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import styles from "./styles";
 import { useState } from "react";
 import Button, { ButtonType } from "../../components/Button";
 
 const Home = () => {
   const [value, setValue] = useState("0");
+  const [firstValue, setFirstValue] = useState("0");
+  const [secondValue, setSecondValue] = useState("0");
+  const [isSecond, setIsSecond] = useState(false);
+  const [operator, setOperator] = useState("");
 
-  const changeValue = (input: string) => {
-    let newValue: string;
-    if (value === "0") {
-      newValue = input;
-    } else {
-      newValue = value.concat(input);
+  const computeInput = (input: string) => {
+    if (input === "+/-") {
+      setValue(returnOposite(value));
+      return;
     }
-    setValue(newValue);
+    if (input === ".") {
+      if (value.includes(".")) {
+        return;
+      } else {
+        let newValue = value.concat(input);
+        setValue(newValue);
+        return;
+      }
+    }
+    if (value === "0") {
+      setValue(input);
+      return;
+    }
+    if (isOperator(input)) {
+      setOperator(input);
+      setIsSecond(true);
+      setFirstValue(value);
+      return;
+    } else if (isSecond) {
+      if (secondValue === "0") {
+        setValue(input);
+        setSecondValue(input);
+      } else {
+        let newValue = value.concat(input);
+        setSecondValue(newValue);
+        setValue(newValue);
+      }
+    } else {
+      let newValue = value.concat(input);
+      setValue(newValue);
+      setFirstValue(newValue);
+    }
+  };
+
+  const isOperator = (input: string) => {
+    return (
+      input === "+" ||
+      input === "-" ||
+      input === "*" ||
+      input === "/" ||
+      input === "%"
+    );
+  };
+
+  const returnOposite = (input: string) => {
+    if (input.includes("-")) {
+      let newValue = input.replace("-", "");
+      return newValue;
+    } else {
+      let newValue = "-".concat(input);
+      return newValue;
+    }
+  };
+
+  const calculate = () => {
+    let result: number;
+    switch (operator) {
+      case "+":
+        result = parseFloat(firstValue) + parseFloat(secondValue);
+        break;
+      case "-":
+        result = parseFloat(firstValue) - parseFloat(secondValue);
+        break;
+      case "*":
+        result = parseFloat(firstValue) * parseFloat(secondValue);
+        break;
+      case "/":
+        result = parseFloat(firstValue) / parseFloat(secondValue);
+        break;
+      case "%":
+        result = (parseFloat(firstValue) / 100) * parseFloat(secondValue);
+        break;
+      default:
+        result = 0;
+        break;
+    }
+    setValue(result.toString());
+    setIsSecond(false);
+    setFirstValue(result.toString());
+    setSecondValue("0");
   };
 
   const deleteInput = () => {
@@ -32,7 +113,9 @@ const Home = () => {
     <View style={styles.container}>
       {/* Calculed value div */}
       <View style={styles.valueContainer}>
-        <Text style={styles.valueText}>{value}</Text>
+        <ScrollView horizontal>
+          <Text style={styles.valueText}>{value}</Text>
+        </ScrollView>
       </View>
       {/* Buttons div */}
       <View style={styles.buttonsContainer}>
@@ -49,19 +132,25 @@ const Home = () => {
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4E505F"
-            onPress={() => {}}
-            text="C"
+            onPress={() => {
+              computeInput("+/-");
+            }}
+            text="+/-"
           />
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4E505F"
-            onPress={() => {}}
+            onPress={() => {
+              computeInput("%");
+            }}
             text="%"
           />
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4B5EFC"
-            onPress={() => {}}
+            onPress={() => {
+              computeInput("/");
+            }}
             text="/"
           />
         </View>
@@ -70,28 +159,30 @@ const Home = () => {
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("7");
+              computeInput("7");
             }}
             text="7"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("8");
+              computeInput("8");
             }}
             text="8"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("9");
+              computeInput("9");
             }}
             text="9"
           />
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4B5EFC"
-            onPress={() => {}}
+            onPress={() => {
+              computeInput("*");
+            }}
             text="x"
           />
         </View>
@@ -100,28 +191,30 @@ const Home = () => {
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("4");
+              computeInput("4");
             }}
             text="4"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("5");
+              computeInput("5");
             }}
             text="5"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("6");
+              computeInput("6");
             }}
             text="6"
           />
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4B5EFC"
-            onPress={() => {}}
+            onPress={() => {
+              computeInput("-");
+            }}
             text="-"
           />
         </View>
@@ -130,35 +223,49 @@ const Home = () => {
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("1");
+              computeInput("1");
             }}
             text="1"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("2");
+              computeInput("2");
             }}
             text="2"
           />
           <Button
             type={ButtonType.NUMBER}
             onPress={() => {
-              changeValue("3");
+              computeInput("3");
             }}
             text="3"
           />
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4B5EFC"
-            onPress={() => {}}
+            onPress={() => {
+              computeInput("+");
+            }}
             text="+"
           />
         </View>
         {/* Line five */}
         <View style={styles.lineButtonsContainer}>
-          <Button type={ButtonType.NUMBER} onPress={() => {}} text="." />
-          <Button type={ButtonType.NUMBER} onPress={() => {}} text="0" />
+          <Button
+            type={ButtonType.NUMBER}
+            onPress={() => {
+              computeInput(".");
+            }}
+            text="."
+          />
+          <Button
+            type={ButtonType.NUMBER}
+            onPress={() => {
+              computeInput("0");
+            }}
+            text="0"
+          />
           <Button
             type={ButtonType.OPERATOR}
             onPress={() => {
@@ -169,7 +276,9 @@ const Home = () => {
           <Button
             type={ButtonType.OPERATOR}
             buttonColor="#4B5EFC"
-            onPress={() => {}}
+            onPress={() => {
+              calculate();
+            }}
             text="="
           />
         </View>
